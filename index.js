1c48ac2839807aba641e108e1192862b80abcef7
simmy bajaj <simmybajaj1603@gmail.com>
2022-02-21 10:05:50 +0530

    changes

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const port = require('./configs/app.config')
 require('./configs/db.config');
 require('./schemas/index');
 const app = require('./server');
-app.listen(port.SERVER_PORT,() => console.log(`service is running ${port}`))
+app.listen(port.SERVER_PORT,() => console.log(`service is running ${port.SERVER_PORT}`))
